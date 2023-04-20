e1b90da493f39d36ce03a786c1578cc12c1c3835
Pedro Henrique Simão Silva <contatophss11@gmail.com>
2023-04-20 13:15:04 -0300

    changes userROutes

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,35 +2,37 @@ import express from "express";
 const router = express.Router();
 
 // controller
-import {
-  register,
-  getAllUsers,
-  getCurrentUser,
-  getUserById,
-  login,
-  updateUser,
-  deleteUser,
-  getByActor,
-} from "../controllers/UserController";
+// import {
+//   register,
+//   getAllUsers,
+//   getCurrentUser,
+//   getUserById,
+//   login,
+//   updateUser,
+//   deleteUser,
+//   getByActor,
+// } from "../controllers/UserController";
+
+import UserController from "../controllers/UserController";
 
 // middlewares
 import checkToken from "../middlewares/checkToken";
 
 // get current user
-router.get("/", checkToken, getCurrentUser);
+router.get("/", checkToken, UserController.getCurrentUser);
 // get all users
-router.get("/all", checkToken, getAllUsers);
+router.get("/all", checkToken, UserController.getAllUsers);
 //register user
-router.post("/register", register);
+router.post("/register", UserController.register);
 // login
-router.post("/login", login);
+router.post("/login", UserController.login);
 // get user by id
-router.get("/:id", checkToken, getUserById);
+router.get("/:id", checkToken, UserController.getUserById);
 // update user
-router.put("/:id", checkToken, updateUser);
+router.put("/:id", checkToken, UserController.updateUser);
 // delete a user
-router.delete("/:id", checkToken, deleteUser);
+router.delete("/:id", checkToken, UserController.deleteUser);
 // get by actor
-router.get("/actor/:value", checkToken, getByActor);
+router.get("/actor/:value", checkToken, UserController.getByActor);
 
 module.exports = router;
